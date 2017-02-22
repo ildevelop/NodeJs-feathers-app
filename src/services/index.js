5b245352c5ab7621f8fb322f46ee288a967610e8
Ilya Radu <raduilya@gmail.com>
2017-02-22 12:48:35 +0200

    Message service

--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,17 @@
 'use strict';
+const messages = require('./messages');
+const contacts = require('./contacts');
 const authentication = require('./authentication');
 const user = require('./user');
 const mongoose = require('mongoose');
 module.exports = function() {
   const app = this;
-  
+
   mongoose.connect(app.get('mongodb'));
   mongoose.Promise = global.Promise;
-  
+
   app.configure(authentication);
   app.configure(user);
+  app.configure(contacts);
+  app.configure(messages);
 };
